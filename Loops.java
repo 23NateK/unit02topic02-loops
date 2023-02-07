71d725e5c4dcdbb5d5bc9b916ca03b473233b16a
23NateK <n.kimber22@gmail.com>
2023-02-07 12:14:43 -0600

    getFactors Finished

--- Loops.java
+++ Loops.java
@@ -1,6 +1,6 @@
+import java.util.Arrays;
 public class Loops {
 
-  //nHellos
 public static void nHellos(int a){
   int num = a;
   for (int n = 1 ; n<=num ; n++){
@@ -25,6 +25,7 @@ public static void nRandoms(int length){
   result = Math.round(result*Math.pow(10, length))/Math.pow(10, length);
   System.out.println(result);
 }
+
 public static boolean isPrime(int integer){
   boolean bool = true;
   if(integer%2==0||integer%3==0||integer%5==0||integer%7==0){
@@ -33,10 +34,27 @@ public static boolean isPrime(int integer){
   return bool;
 }
 
+public static int[] getFactors(int integer){
+  int [] listnums = new int[0];
+    for (int n = 1; n <= integer; n++){
+      if (integer%n==0){
+        int [] copyList = new int[listnums.length+1];
+        for(int x = 0; x< listnums.length; x++){
+          copyList[x] = listnums[x];
+        }
+        copyList[copyList.length-1] = n;
+        listnums = copyList;
+      }
+    }
+  return listnums;
+}
+
+
 
   public static void main(String[] args) {
     //nHellos(150);
     //nRandoms(10);
-    System.out.println(isPrime(59));
+    //System.out.println(isPrime(59));
+    System.out.println(Arrays.toString(getFactors(24)));
   }
 }
